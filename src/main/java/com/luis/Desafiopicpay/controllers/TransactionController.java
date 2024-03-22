c6bf4ddf2a55b6e065f8db78208f20e8d627f24f
Luis Fernando <luis.almeida.faria@gmail.com>
2024-03-21 22:56:57 -0300

    Implementação da lógica da transactionController

--- src/main/java/com/luis/Desafiopicpay/controllers/TransactionController.java
+++ src/main/java/com/luis/Desafiopicpay/controllers/TransactionController.java
@@ -1,2 +1,23 @@
-package com.luis.Desafiopicpay.controllers;public class TransactionController {
+package com.luis.Desafiopicpay.controllers;
+
+import com.luis.Desafiopicpay.domain.transaction.Transaction;
+import com.luis.Desafiopicpay.dtos.TransactionDTO;
+import com.luis.Desafiopicpay.services.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/transactions")
+public class TransactionController {
+    @Autowired
+    private TransactionService transactionService;
+
+    @PostMapping
+    public ResponseEntity<Transaction> createTransaction(@RequestBody TransactionDTO transaction) throws Exception {
+        Transaction newTransaction = this.transactionService.createTransaction(transaction);
+        return new ResponseEntity<>(newTransaction, HttpStatus.OK);
+    }
+
 }
